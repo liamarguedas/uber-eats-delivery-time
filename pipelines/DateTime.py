2c4c08a444a135502e296c09d0bf1c55d56a836b
liamarguedas <iliamftw2013@gmail.com>
2023-06-13 18:05:52 -0300

    Numeric EAD Finished

--- pipelines/DateTime.py
+++ pipelines/DateTime.py
@@ -4,6 +4,8 @@ def DateTime(data: pd.DataFrame):
 	data['Time_Orderd'] = pd.to_datetime(data['Time_Orderd'], format = '%d-%m-%Y %H:%M:%S')
 	data['Time_Order_picked'] = pd.to_datetime(data['Time_Order_picked'], format = '%d-%m-%Y %H:%M:%S')
 	data['Time_To_Pick'] = (data['Time_Order_picked'] - data['Time_Orderd']).astype('timedelta64[m]')
+	data['Time_To_Pick'] = data['Time_To_Pick'].apply(
+    lambda value: value if value not in [-1425.0, -1430.0, -1435.0] else 15.0 if value == -1425.0 else 10.0 if value == -1430.0 else 5.0)
 	data['Order_Year'] = data['Time_Orderd'].apply(lambda value: value.year)
 	data['Order_Month'] = data['Time_Orderd'].apply(lambda value: value.month).map({1:'January', 2:'February',
 	3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October',11:'November', 12:'December'})
