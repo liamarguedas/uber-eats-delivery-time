d208b126cece9b2bdb47b878dd3768130928493f
liamarguedas <iliamftw2013@gmail.com>
2023-06-16 13:59:34 -0300

    EAD Finished

--- pipelines/DateTime.py
+++ pipelines/DateTime.py
@@ -11,4 +11,6 @@ def DateTime(data: pd.DataFrame):
 	3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October',11:'November', 12:'December'})
 	data['Order_Day'] = data['Time_Orderd'].apply(lambda value: value.dayofweek).map({0:'Monday', 1:'Tuesday', 2:'Wednesday',
 	3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'})
+	data['OrderTime'] = pd.to_datetime(data['Time_Order_picked']).apply(lambda value: value.hour)
+	data['TypeOfMeal'] = data['OrderTime'].apply(lambda value: "Breakfast" if value in [8, 9, 10, 11] else "Launch" if value in [12, 13, 14, 15, 16] else "Dinner")
 	return data
